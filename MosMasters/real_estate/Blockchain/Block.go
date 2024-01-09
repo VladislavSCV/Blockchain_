6284b4e833dec559d9dbe7862163c0f67a90cd37
VLADislav676 <116951315+VladislavSCV@users.noreply.github.com>
2024-01-09 19:14:33 +0300

    Update Block.go

--- MosMasters/real_estate/Blockchain/Block.go
+++ MosMasters/real_estate/Blockchain/Block.go
@@ -3,15 +3,23 @@ package Blockchain
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
+	"math/big"
 	"strconv"
 	"time"
 )
+
+const targetBits = 12
+const maxNonce = 1<<63-1
 
 type Block struct {
 	Timestamp int64
 	Data []byte
 	PrevBlockHash []byte
 	Hash []byte
+	Nonce int
 }
 
 func (b *Block) SetHash() {
@@ -23,7 +31,111 @@ func (b *Block) SetHash() {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-    block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-    block.SetHash()
-    return block
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
 }
+
+/* POW */
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			[]byte(strconv.FormatInt(pow.block.Timestamp, 10)),
+			[]byte(strconv.FormatInt(int64(targetBits), 10)),
+			[]byte(strconv.FormatInt(int64(nonce), 10)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+
+/* POW */
+
+/* Validation */
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
+
+/* Validation */
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic("Ошибка с сериализацией")
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic("Ошибка с десериализицией")
+	}
+
+	return &block
+}
+
+
